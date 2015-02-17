2203d8de1c2cf964183a4e437abf578f4fba7722
Zuestomon <higgsbo@gmail.comn>
2015-02-16 16:34:15 -0800

    spell-checked comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ## Initial stub commit to own git repo 
-## maCacheMatrix creates a function that stores the values of an inverted matrix
+## mCacheMatrix creates a function that stores the values of an inverted matrix
 ## The second function takes Fn1 input and determines if the inverted matrix exists in cache. If yes use that value, if not then use solve(<matrix>) to generate in
 
 ## Write a short comment describing this function
