7ce42d983cb7516b88118bc5ee27cf6420122de1
Zuestomon <higgsbo@gmail.comn>
2015-02-16 16:28:11 -0800

    initial stub update

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Initial stub commit to own git repo 
+## maCacheMatrix creates a function that stores the values of an inverted matrix
+## The second function takes Fn1 input and determines if the inverted matrix exists in cache. If yes use that value, if not then use solve(<matrix>) to generate in
 
 ## Write a short comment describing this function
 
